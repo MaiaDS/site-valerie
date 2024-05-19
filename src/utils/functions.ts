f938d79b32eeb7e9279d3b42e57fea7f87dd1e99
Maia Da Silva <60962215+MaiaDS@users.noreply.github.com>
2024-05-18 20:57:50 -0400

    refactor blog pages + use api + fix style

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -1,7 +1,17 @@
+import { sanityClient } from 'sanity:client'
+import imageUrlBuilder from '@sanity/image-url'
+import type { ImageAsset } from 'sanity'
+
 export const formatDate = (date: Date) => {
     return date.toLocaleDateString('fr-FR', {
         year: 'numeric',
         month: 'short',
         day: 'numeric',
     })
+}
+
+export const getImageSrc = (source: ImageAsset | undefined) => {
+    if (!source) return undefined
+    const imageBuilder = imageUrlBuilder(sanityClient)
+    return imageBuilder.image(source).url()
 }
