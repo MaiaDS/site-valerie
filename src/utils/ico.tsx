d31a5d437139c230d82d33896823c90ff5efcbdb
Maia Da Silva <60962215+MaiaDS@users.noreply.github.com>
2024-05-20 16:11:47 -0400

    add footer ico

---
+++ src/utils/ico.tsx
@@ -0,0 +1,45 @@
+export const EmailIco = () => {
+    return (
+        <svg
+            width="18"
+            height="14"
+            viewBox="0 0 18 14"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg">
+            <path
+                d="M1 3.42857L8.21231 7.45627C8.43346 7.57683 8.71218 7.64286 9 7.64286C9.28782 7.64286 9.56654 7.57683 9.78769 7.45627L17 3.42857M15.7692 1H2.23077C1.55103 1 1 1.51168 1 2.14286V11.8571C1 12.4883 1.55103 13 2.23077 13H15.7692C16.449 13 17 12.4883 17 11.8571V2.14286C17 1.51168 16.449 1 15.7692 1Z"
+                className="stroke-black"
+                stroke-width="1.5"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+            />
+        </svg>
+    )
+}
+
+export const InstagramIco = () => {
+    return (
+        <svg
+            width="18"
+            height="18"
+            viewBox="0 0 18 18"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg">
+            <circle cx="13.625" cy="4.38027" r="1.1" className="fill-black" />
+            <path
+                d="M1 4.42857C1 2.53503 2.53502 1 4.42857 1H13.5715C15.465 1 17 2.53503 17 4.42857V13.5714C17 15.4649 15.465 17 13.5715 17H4.42857C2.53502 17 1 15.4649 1 13.5714V4.42857Z"
+                className="stroke-black"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+            />
+            <path
+                d="M9.00003 12.3978C10.8766 12.3978 12.3979 10.8765 12.3979 8.99987C12.3979 7.12326 10.8766 5.60196 9.00003 5.60196C7.12342 5.60196 5.60213 7.12326 5.60213 8.99987C5.60213 10.8765 7.12342 12.3978 9.00003 12.3978Z"
+                className="stroke-black"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+            />
+        </svg>
+    )
+}
