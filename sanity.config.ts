4b88bdd691577cc1df78d876e8be2b2614c12978
Maia Da Silva <60962215+MaiaDS@users.noreply.github.com>
2024-05-19 22:30:00 -0400

    update sanity config to set up singletons

--- sanity.config.ts
+++ sanity.config.ts
@@ -3,6 +3,9 @@ import { structureTool } from 'sanity/structure'
 import { visionTool } from '@sanity/vision'
 import { schemaTypes } from './schema'
 
+const singletonActions = new Set(['publish', 'discardChanges', 'restore'])
+const singletonTypes = new Set(['homepage', 'settings'])
+
 export default defineConfig({
     name: 'default',
     title: 'Valérie Da Silva',
@@ -10,9 +13,50 @@ export default defineConfig({
     projectId: 'chqqc08z',
     dataset: 'production',
 
-    plugins: [structureTool(), visionTool()],
+    plugins: [
+        structureTool({
+            structure: (S) =>
+                S.list()
+                    .title('Content')
+                    .items([
+                        S.listItem()
+                            .title('Global')
+                            .id('settings')
+                            .child(
+                                S.document()
+                                    .schemaType('settings')
+                                    .documentId('settings'),
+                            ),
+                        S.listItem()
+                            .title("Page d'accueil")
+                            .id('homepage')
+                            .child(
+                                S.document()
+                                    .schemaType('homepage')
+                                    .documentId('homepage'),
+                            ),
+                        // Regular document types
+                        S.documentTypeListItem('page').title('Pages'),
+                        S.documentTypeListItem('post').title('Articles'),
+                    ]),
+        }),
+        visionTool(),
+    ],
 
     schema: {
         types: schemaTypes,
+        templates: (templates) =>
+            templates.filter(
+                ({ schemaType }) => !singletonTypes.has(schemaType),
+            ),
+    },
+
+    document: {
+        actions: (input, context) =>
+            singletonTypes.has(context.schemaType)
+                ? input.filter(
+                      ({ action }) => action && singletonActions.has(action),
+                  )
+                : input,
     },
 })
